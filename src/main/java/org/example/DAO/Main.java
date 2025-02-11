98402af3b99c42f5a612aec7198b4b21710a24b2
mihow201 <phamtattu201@gmail.com>
2025-02-11 23:28:25 +0700

    tiep

--- src/main/java/org/example/DAO/Main.java
+++ src/main/java/org/example/DAO/Main.java
@@ -5,6 +5,6 @@ import org.example.VIEW.HomeJFrame;
 public class Main {
     public static void main(String[] args) {
         new HomeJFrame().setVisible(true);
-        System.out.println("hi");
+        System.out.println("hi chinh");
     }
 }
