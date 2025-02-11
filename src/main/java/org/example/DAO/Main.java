9a3167fa487c46e44d4a933dbfd94f9fedf6a652
Mainca123 <giangchinhtgp@gmail.com>
2025-02-11 23:19:49 +0700

    test 3

--- src/main/java/org/example/DAO/Main.java
+++ src/main/java/org/example/DAO/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
         new HomeJFrame().setVisible(true);
         System.out.println("Hello");
+        if(2)
+            System.out.println("hihi");
     }
 }
