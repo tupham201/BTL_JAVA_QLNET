01381d68e7a3757f049528ade0c5221a52bc6304
mihow201 <phamtattu201@gmail.com>
2025-02-11 23:15:39 +0700

    test tiep

--- src/main/java/org/example/DAO/Main.java
+++ src/main/java/org/example/DAO/Main.java
@@ -5,6 +5,6 @@ import org.example.VIEW.HomeJFrame;
 public class Main {
     public static void main(String[] args) {
         new HomeJFrame().setVisible(true);
-        System.out.println("Hello");
+        System.out.println("hi");
     }
 }
